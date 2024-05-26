6a146f1ea4d78fae33d242e33176f13bebc5f892
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2024-05-26 20:14:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,11 +20,11 @@ app.get('/play', async (req, res) => {
       format: audioFormat
     }).pipe(res);
   } catch (error) {
-    res.status(400).send(error.message);
+    console.error(error);
+    res.status(500).send('Something went wrong!');
   }
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-

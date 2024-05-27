b170b0f05ac336fdea4abf199584026704bf0233
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2024-05-27 14:26:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,29 +6,18 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-let command = '';
-let audioUrl = '';
-
-app.post('/send-command', (req, res) => {
-    command = req.body.command;
-    res.json({ status: 'Command received' });
-});
+let iframeUrl = '';
 
 app.post('/update-url', (req, res) => {
-    audioUrl = req.body.url;
+    iframeUrl = req.body.url;
     res.json({ status: 'URL updated' });
 });
 
-app.get('/get-command', (req, res) => {
-    res.json({ command });
-});
-
-app.get('/get-url', (req, res) => {
-    res.json({ url: audioUrl });
+app.get('/current-url', (req, res) => {
+    res.json({ url: iframeUrl });
 });
 
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
